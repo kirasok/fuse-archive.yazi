e12180587ee67fb716f7ea9661c313cbb99f9399
Huy Hoang <boydaihungst@gmail.com>
2024-10-21 19:56:27 +0700

    feat: Added mount encrypted archive, notify for errors, multiple deep mount
    BREAKING CHANGE: You need to use the fuse-archive from git [fuse-archive](https://github.com/google/fuse-archive)
    ```sh
    git clone https://github.com/google/fuse-archive
    cd "fuse-archive"
    make install
    ```
    BREAKING CHANGE: `plugin fuse-archive --args=unmount` key in keymap.toml should changed to `plugin fuse-archive --args=leave`
    to make multiple deep mount work (e.g. mount zip in in a zip). the
    `unmount` still there if you want to unmount after leave the folder
    (this won't let you copy/move files/folders to other place without create another
    tab). But the downside of `leave` command is that the zip file won't unmount
    itself after exit yazi. Maybe use another shellscript with `trap EXIT` `uuidgen` and `umount` could solve this problem. I'll take a look at it later

--- init.lua
+++ init.lua
@@ -1,7 +1,100 @@
-local shell = os.getenv("SHELL")
+---@enum FUSE_ARCHIVE_RETURN_CODE
+local FUSE_ARCHIVE_RETURN_CODE = {
+	SUCCESS = 0, -- Success.
+	ERROR_GENERIC = 1, -- Generic error code for: missing command line argument, \
+	-- too many command line arguments, unknown option, mount point is not empty, etc.
+	CREATE_MOUNT_POINT_FAILED = 10, -- Cannot create the mount point.
+	OPEN_THE_ACHIVE_FILE_FAILED = 11, -- Cannot open the archive file.
+	CREATE_CACHE_FILE_FAILED = 12, -- Cannot create the cache file.
+	NOT_ENOUGH_TEMP_SPACE = 13, -- Cannot write to the cache file. This is most likely the indication that there is not enough temp space.
+	ENCRYPTED_FILE_BUT_NOT_PASSWORD = 20, -- The archive contains an encrypted file, but no password was provided.
+	ENCRYPTED_FILE_BUT_WRONG_PASSWORD = 21, -- The archive contains an encrypted file, and the provided password does not decrypt it.
+	ENCRYPTED_METHOD_UNSUPPORTED = 22, -- The archive contains an encrypted file, and the encryption method is not supported.
+	ARCHIVE_FORMAT_UNSUPPORTED = 30, -- Cannot recognize the archive format.
+	ARCHIVE_HEADER_INVALID = 31, -- Invalid archive header.
+	ARCHIVE_READ_PERMISSION_INVALID = 32, -- Cannot read and extract the archive.
+}
+
+---@enum FUSE_ARCHIVE_MOUNT_ERROR_MSG
+local FUSE_ARCHIVE_MOUNT_ERROR_MSG = {
+	[FUSE_ARCHIVE_RETURN_CODE.ERROR_GENERIC] = "Fuse-archive exited with error: %s", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.CREATE_MOUNT_POINT_FAILED] = "Can't create mount point %s, maybe you don't have permission", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.OPEN_THE_ACHIVE_FILE_FAILED] = "Can't open archive file, maybe you don't have permission", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.CREATE_CACHE_FILE_FAILED] = "Can't not create cache point or not enough space for cache, trying to disable cache opt, this would make thing much slower", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.NOT_ENOUGH_TEMP_SPACE] = "Can't not create cache point or not enough space for cache, trying to disable cache opt, this would make thing much slower", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.ENCRYPTED_METHOD_UNSUPPORTED] = "Encrypted method is unsupported", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.ENCRYPTED_FILE_BUT_WRONG_PASSWORD] = "Incorrect password, %s attempts remaining.", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.ENCRYPTED_FILE_BUT_NOT_PASSWORD] = "Please enter password to unlock file,%s attempts remaining.", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.ARCHIVE_FORMAT_UNSUPPORTED] = "Unsupported this format file", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.ARCHIVE_HEADER_INVALID] = "Archive file is corrupted", -- Success.
+	[FUSE_ARCHIVE_RETURN_CODE.ARCHIVE_READ_PERMISSION_INVALID] = "Can't open archive file, maybe you don't have permission",
+}
+---@enum YA_INPUT_EVENT
+local YA_INPUT_EVENT = {
+	ERROR = 0,
+	CONFIRMED = 1,
+	CANCELLED = 2,
+	VALUE_CHANGED = 3,
+}
+
+---@enum Command.PIPED
+---@enum Command.NULL
+---@enum Command.INHERIT
+
+---@type Command
+local Command = _G.Command
+
+---@class (exact) Command
+---@overload fun(cmd: string): self
+---@field PIPED Command.PIPED
+---@field NULL Command.NULL
+---@field INHERIT Command.INHERIT
+---@field arg fun(self: Command, arg: string): self
+---@field args fun(self: Command, args: string[]): self
+---@field cwd fun(self: Command, dir: string): self
+---@field env fun(self: Command, key: string, value: string): self
+---@field stdin fun(self: Command, cfg: Command.PIPED | Command.NULL | Command.INHERIT| STD_STREAM): self
+---@field stdout fun(self: Command, cfg: Command.PIPED | Command.NULL | Command.INHERIT| STD_STREAM): self
+---@field stderr fun(self: Command, cfg: Command.PIPED | Command.NULL | Command.INHERIT| STD_STREAM): self
+---@field spawn fun(self: Command): Child|nil, integer
+---@field output fun(self: Command): Output|nil, integer
+---@field status fun(self: Command): Status|nil, integer
+
+---@alias STD_STREAM unknown
+
+---@class (exact) Child
+---@field read fun(self: Child, len: string): string, 1|0
+---@field read_line fun(self: Child): string, 1|0
+---@field read_line_with fun(self: Child, opts: {timeout: integer}): string, 1|2|3
+---@field wait fun(self: Child): Status|nil, integer
+---@field wait_with_output fun(self: Child): Output|nil, integer
+---@field start_kill fun(self: Child): boolean, integer
+--- stdin(Command.PIPED) is set
+---@field take_stdin fun(self: Child): STD_STREAM|nil, integer
+--- stdin(Command.PIPED) is set
+---@field take_stdout fun(self: Child): STD_STREAM|nil, integer
+--- stdin(Command.PIPED) is set
+---@field take_stderr fun(self: Child): STD_STREAM|nil, integer
+--- stdin(Command.PIPED) is set
+--- take_stdin() has never been called
+---@field write_all fun(self: Child, src: string): STD_STREAM|nil, integer
+---@field flush fun(self: Child): STD_STREAM|nil, integer
+
+---@class (exact) Output The Output of the command if successful; otherwise, nil
+---@field status Status The Status of the child process
+---@field stdout string The stdout of the child process, which is a string
+---@field stderr string The stderr of the child process, which is a string
+
+---@class (exact) Status The Status of the child process
+---@field success boolean whether the child process exited successfully, which is a boolean.
+---@field code integer the exit code of the child process, which is an integer if any
 
 local function error(s, ...)
 	ya.notify({ title = "fuse-archive", content = string.format(s, ...), timeout = 3, level = "error" })
+end
+
+local function info(s, ...)
+	ya.notify({ title = "fuse-archive", content = string.format(s, ...), timeout = 3, level = "info" })
 end
 
 local set_state = ya.sync(function(state, archive, key, value)
@@ -31,15 +124,6 @@ local is_mount_point = ya.sync(function(state)
 	return false
 end)
 
-local smart_enter = ya.sync(function()
-	local exist, smart_enter = pcall(require, "smart-enter")
-	if exist then
-		smart_enter.entry()
-	else
-		ya.manager_emit("enter", {})
-	end
-end)
-
 local current_file = ya.sync(function()
 	local h = cx.active.current.hovered
 	if h then
@@ -57,26 +141,54 @@ local current_dir_name = ya.sync(function()
 	return cx.active.current.cwd:name()
 end)
 
-local function run_command(cmd, args)
+local enter = ya.sync(function()
+	local h = cx.active.current.hovered
+	if h then
+		if h.cha.is_dir then
+			ya.manager_emit("enter", {})
+		else
+			if get_state("global", "smart_enter") then
+				ya.manager_emit("open", { hovered = true })
+			else
+				ya.manager_emit("enter", {})
+			end
+		end
+	end
+end)
+
+---run any command
+---@param cmd string
+---@param args string[]
+---@param _stdin? STD_STREAM|nil
+---@return integer|nil, Output|nil
+local function run_command(cmd, args, _stdin)
 	local cwd = current_dir()
+	local stdin = _stdin or Command.INHERIT
 	local child, cmd_err =
-		Command(cmd):args(args):cwd(cwd):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
+		Command(cmd):args(args):cwd(cwd):stdin(stdin):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()
 
 	if not child then
-		error("Spawn `fuse-archive` failed with error code %s", cmd_err)
-		return cmd_err
+		error("Spawn " .. cmd .. " failed with error code %s", cmd_err)
+		return cmd_err, nil
 	end
 
 	local output, out_err = child:wait_with_output()
 	if not output then
-		error("Cannot read `fuse-archive` output, error code %s", out_err)
-		return out_err
-	elseif not output.status.success then
-		error("`fuse-archive` exited with error code %s", output.status.code)
-		return output.status.code
+		error("Cannot read " .. cmd .. " output, error code %s", out_err)
+		return out_err, nil
 	else
-		return 0
+		return nil, output
 	end
+end
+
+local is_mounted = function(dir_path)
+	ya.err(dir_path)
+	local cmd_err_code, res = run_command("mountpoint", { "-q", "--", ya.quote(dir_path) })
+	if cmd_err_code or res == nil or res.status.code ~= 0 then
+		-- case error, or mountpoint command not found
+		return false
+	end
+	return res and res.status.success
 end
 
 local valid_extension = ya.sync(function()
@@ -110,9 +222,9 @@ local valid_extension = ya.sync(function()
 			"xpi",
 			"cab",
 		}
-		local filename = tostring(h.url)
+		local file_extention = h.url.ext(h.url)
 		for _, ext in ipairs(valid_extensions) do
-			if filename:find("%." .. ext .. "$") then
+			if ext == file_extention then
 				return true
 			end
 		end
@@ -122,39 +234,190 @@ local valid_extension = ya.sync(function()
 	end
 end)
 
+---Get the fuse mount point
+---@return string
 local function fuse_dir()
-	local state_dir = os.getenv("XDG_STATE_HOME")
-	if not state_dir then
-		local home = os.getenv("HOME")
-		if not home then
-			state_dir = "/tmp"
-		else
-			state_dir = home .. "/" .. ".local/state"
-		end
-	end
+	local state_dir = "/tmp"
 	return state_dir .. "/yazi/fuse-archive"
 end
 
-local function get_tmp_file_name(path)
-	local time_now = os.time()
-	local hex_time = string.format("%x", time_now)
-	return path .. ".tmp" .. hex_time
+--- return a string array with unique value
+---@param tbl string[]
+---@return string[] table with only unique strings
+function tbl_unique_strings(tbl)
+	local unique_table = {}
+	local seen = {}
+
+	for _, str in ipairs(tbl) do
+		if not seen[str] then
+			seen[str] = true
+			table.insert(unique_table, str)
+		end
+	end
+
+	return unique_table
 end
 
-local function create_mount_path(file)
+---
+---@param file string file path
+---@return string
+local function get_mount_path(file)
 	local tmp_path = get_state("global", "fuse_dir") .. "/" .. file
-
-	local ret_code = run_command("mkdir", { "-p", tmp_path })
-	if ret_code ~= 0 then
-		error("Cannot create tmp file %s", tmp_path)
-		return nil
-	end
 	return tmp_path
 end
 
-local function setup()
+---Show password input dialog
+---@return boolean cancelled, string password
+local function show_ask_pw_dialog()
+	---TODO: update this with input password (*******)
+	local passphrase = ""
+	local cancelled = false
+	-- Asking user to input the password
+	local input_pw = ya.input({
+		title = "Enter password to unlock:",
+		position = { "center", x = 0, y = 0, w = 50, h = 3 },
+		realtime = true,
+	})
+
+	while true do
+		---@type string, YA_INPUT_EVENT
+		local value, ev = input_pw:recv()
+		if ev == YA_INPUT_EVENT.CONFIRMED then
+			passphrase = value or ""
+			break
+		elseif ev == YA_INPUT_EVENT.CANCELLED then
+			passphrase = ""
+			cancelled = true
+			break
+		end
+	end
+	return cancelled, passphrase
+end
+
+---mount fuse
+---@param opts {archive_path: string, fuse_mount_point: string, mount_opts: string[], passphrase?: string, max_retry?: integer, retries?: integer}
+---@return boolean
+local function mount_fuse(opts)
+	local archive_path = opts.archive_path
+	local fuse_mount_point = opts.fuse_mount_point
+	local mount_opts = opts.mount_opts
+	local passphrase = opts.passphrase
+	local max_retry = opts.max_retry or 3
+	local retries = opts.retries or 0
+	local ignore_global_error_notify = false
+	local payload_error_notify = {}
+
+	if is_mounted(opts.fuse_mount_point) then
+		ya.err("already mounted, skipping")
+		return true
+	end
+	local passpharase_stdin = Command.PIPED
+	mount_opts = tbl_unique_strings({ "auto_unmount", "use_ino", "readdir_ino", table.unpack(mount_opts or {}) })
+
+	-- show notify + exit after too many attempts to mount
+
+	if passphrase then
+		passpharase_stdin =
+			Command("echo"):arg(ya.quote(passphrase)):stderr(Command.PIPED):stdout(Command.PIPED):spawn():take_stdout()
+	end
+	local res, _ = Command("fuse-archive")
+		:args({
+			"-o",
+			table.concat(mount_opts, ","),
+			ya.quote(archive_path),
+			ya.quote(fuse_mount_point),
+		})
+		:stderr(Command.PIPED)
+		:stdin(passpharase_stdin)
+		:stdout(Command.NULL)
+		:output()
+
+	local fuse_mount_res_code
+
+	-- already mounted, so stop re-mount
+	if res then
+		if res.stderr and res.stderr:find("mountpoint is not empty") then
+			return true
+		end
+		fuse_mount_res_code = res.status.code
+	end
+
+	if fuse_mount_res_code == FUSE_ARCHIVE_RETURN_CODE.SUCCESS then
+		return true
+	elseif fuse_mount_res_code == FUSE_ARCHIVE_RETURN_CODE.ERROR_GENERIC then
+		payload_error_notify = { fuse_mount_res_code }
+	elseif fuse_mount_res_code == FUSE_ARCHIVE_RETURN_CODE.CREATE_MOUNT_POINT_FAILED then
+		payload_error_notify = { fuse_mount_point }
+	elseif
+		fuse_mount_res_code == FUSE_ARCHIVE_RETURN_CODE.NOT_ENOUGH_TEMP_SPACE
+		or fuse_mount_res_code == FUSE_ARCHIVE_RETURN_CODE.CREATE_CACHE_FILE_FAILED
+	then
+		-- disable cache
+		table.insert(mount_opts, "nocache")
+	elseif
+		fuse_mount_res_code == FUSE_ARCHIVE_RETURN_CODE.ENCRYPTED_FILE_BUT_NOT_PASSWORD
+		or fuse_mount_res_code == FUSE_ARCHIVE_RETURN_CODE.ENCRYPTED_FILE_BUT_WRONG_PASSWORD
+	then
+		ignore_global_error_notify = true
+		-- Too many attempts
+		if retries == max_retry then
+			return false
+		end
+		if retries == 0 then
+			-- First time ask for password dialog shown up
+			info(FUSE_ARCHIVE_MOUNT_ERROR_MSG[fuse_mount_res_code], max_retry - retries)
+		else
+			error(FUSE_ARCHIVE_MOUNT_ERROR_MSG[fuse_mount_res_code], max_retry - retries)
+		end
+		local cancelled, pw = show_ask_pw_dialog()
+		if not cancelled then
+			passphrase = pw
+		else
+			return false
+		end
+	end
+
+	--show retry notification
+	if retries >= max_retry or not ignore_global_error_notify then
+		if FUSE_ARCHIVE_MOUNT_ERROR_MSG[fuse_mount_res_code] then
+			error(FUSE_ARCHIVE_MOUNT_ERROR_MSG[fuse_mount_res_code], table.unpack(payload_error_notify))
+		end
+		return false
+	end
+	-- Increase retries every run
+	retries = retries + 1
+	return mount_fuse({
+		archive_path = archive_path,
+		fuse_mount_point = fuse_mount_point,
+		mount_opts = mount_opts,
+		passphrase = passphrase,
+		retries = retries,
+		max_retry = max_retry,
+	})
+end
+
+---Mount path using inode (unique for each files)
+---e.g. /tmp/yazi/fuse-archive/test.zip.tmp.11675995
+---@param path string
+---@return string|nil
+local function get_file_name_with_inode(path)
+	local cmd_err_code, res = run_command("ls", { "-i", path })
+	if cmd_err_code or res == nil or res.status.code ~= 0 then
+		error("Cannot get inode of file %s", path)
+		return nil
+	end
+	local inode = res.stdout:match("^(%d+)")
+	return path .. ".tmp." .. tostring(inode)
+end
+
+local function setup(_, opts)
 	local fuse = fuse_dir()
 	set_state("global", "fuse_dir", fuse)
+	if opts and opts.smart_enter then
+		set_state("global", "smart_enter", true)
+	else
+		set_state("global", "smart_enter", false)
+	end
 end
 
 return {
@@ -170,28 +433,35 @@ return {
 				return
 			end
 			if not valid_extension() then
-				smart_enter()
+				enter()
 				return
 			end
-			local tmp_file_name = get_tmp_file_name(file)
-			local tmp_file_path = create_mount_path(tmp_file_name)
-			if not tmp_file_path then
-				return
-			end
-			local ret_code =
-				run_command(shell, { "-c", "fuse-archive " .. ya.quote(file) .. " " .. ya.quote(tmp_file_path) })
-			if ret_code ~= 0 then
-				os.remove(tmp_file_path)
-				error(" Unable to mount %s", file)
-				return
-			end
-			set_state(tmp_file_name, "cwd", current_dir())
-			set_state(tmp_file_name, "tmp", tmp_file_path)
-			ya.manager_emit("cd", { tmp_file_path })
-			smart_enter()
-		end
+			local tmp_file_name = get_file_name_with_inode(file)
+			local tmp_file_path = get_mount_path(tmp_file_name)
 
-		if action == "unmount" then
+			if tmp_file_path then
+				local success = mount_fuse({
+					archive_path = "./" .. file,
+					fuse_mount_point = tmp_file_path,
+				})
+				if success then
+					set_state(tmp_file_name, "cwd", current_dir())
+					set_state(tmp_file_name, "tmp", tmp_file_path)
+					ya.manager_emit("cd", { tmp_file_path })
+					ya.manager_emit("enter", {})
+				end
+			end
+			-- leave without unmount
+		elseif action == "leave" then
+			if not is_mount_point() then
+				ya.manager_emit("leave", {})
+				return
+			end
+			local file = current_dir_name()
+			local tmp_file = get_state(file, "tmp")
+			ya.manager_emit("cd", { get_state(file, "cwd") })
+			return
+		elseif action == "unmount" then
 			if not is_mount_point() then
 				ya.manager_emit("leave", {})
 				return
@@ -200,14 +470,9 @@ return {
 			local tmp_file = get_state(file, "tmp")
 			ya.manager_emit("cd", { get_state(file, "cwd") })
 
-			local ret_code = run_command(shell, { "-c", "fusermount -u " .. ya.quote(tmp_file) })
-			if ret_code ~= 0 then
+			local cmd_err_code, res = run_command("umount", { ya.quote(tmp_file) })
+			if cmd_err_code or res and not res.status.success then
 				error("Unable to unmount %s", tmp_file)
-			end
-
-			local deleted, _ = os.remove(tmp_file)
-			if not deleted then
-				error("Cannot delete tmp file %s", tmp_file)
 			end
 			return
 		end
